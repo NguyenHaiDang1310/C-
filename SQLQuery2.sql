241c24fbe09b0b2fe658690a9eccbc8bc6810f83
Nguyễn Hải Đăng <145883675+NguyenHaiDang1310@users.noreply.github.com>
2024-10-19 08:59:58 +0700

    Add files via upload

---
+++ SQLQuery2.sql
@@ -0,0 +1,13 @@
+﻿CREATE DATABASE CustomerDB;
+GO
+
+-- Sử dụng cơ sở dữ liệu CustomerDB
+USE CustomerDB;
+GO
+
+-- Tạo bảng Customers
+CREATE TABLE Customers (
+    Id INT PRIMARY KEY,
+    Name NVARCHAR(100) NOT NULL
+);
+GO
